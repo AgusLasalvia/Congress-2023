048abb242cd7d12a7c9efdf49b152dc0bbbaa276
AgusLasalvia <aguslblumenfeld@gmail.com>
2023-05-23 21:42:45 -0300

    mail method && POST end points routes

--- server/index.js
+++ server/index.js
@@ -1,28 +1,58 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 
-//express conficurations 
+var mail = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "",
+    pass: "",
+  },
+});
+
+//express conficurations
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post('/pre_registration',(req,res)=>{
-  const {
-    email,name,lastname,gender,
-    level_ed,country,institution
-  } = req.body
-
-
+app.post("/pre_registration", (req, res) => {
+  const { email, name, lastname, gender, level_ed, country, institution } =
+    req.body;
 });
 
-
-app.post('/registration',(req,res)=>{
+app.post("/registration", (req, res) => {
   const {
-    email,name,lastname,gender,
-    level_ed,position,main_inst,
-    inst_addrs,country,region,
-    city,postal,modality,sp_meal,
-    first_reg,m_language
-  } = req.body
+    email,
+    name,
+    lastname,
+    gender,
+    level_ed,
+    position,
+    main_inst,
+    inst_addrs,
+    country,
+    region,
+    city,
+    postal,
+    modality,
+    sp_meal,
+    first_reg,
+    m_language,
+  } = req.body;
 });
+
+function SendMail(reciver, attachment) {
+  var mailOptions = {
+    from: "quitel's mail",
+    to: reciver,
+    subject: "example: Payment notification",
+    text: "",
+  };
+  if (attachment) {
+    mailOptions["attachment"] = {
+      filename: attachment,
+      content: new Buffer(attachment, "utf-8"),
+    };
+  }
+}
