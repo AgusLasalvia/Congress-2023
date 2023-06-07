dced34394e452a20df14ae0abd05ae266a0fcb33
AgusLasalvia <@aguslblumenfeld@gmail.com@>
2023-06-07 09:29:40 +0900

    .

--- client/src/Pages/Registration/PreRegistration/PreRegistration.jsx
+++ client/src/Pages/Registration/PreRegistration/PreRegistration.jsx
@@ -5,7 +5,7 @@ import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 import { preRegistration, sendPreRegistration } from "../../../services/FormsService";
-import { validateData } from "../../../hooks/ValidateData";
+import { validateData } from "../../../hooks/validateData";
 
 export default function PreRegistration() {
 
