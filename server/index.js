6a85877baf4a53d0879a9dcf6070869170e7753e
AgusLasalvia <aguslblumenfeld@gmail.com>
2023-06-14 22:50:38 -0300

    .

--- server/index.js
+++ server/index.js
@@ -31,14 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParse.json());
 app.use(cors());
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    safeFileNames: true,
-    preserveExtension: true,
-    tempFileDir: `${__dirname}/public/files/temp`
-  })
-);
+app.use(fileUpload());
 
 
 app.get('/',(req,res)=>{
@@ -88,19 +81,9 @@ app.post('/get_personInfo',(req,res)=>{
 
 app.post("/registration",async (req, res) => {
   const data = req.body;
-  const files = req.files.file;
-  console.log(files)
+  const file = req.files.file;
+  console.log(file)
   console.log(data)
-  const name = uploadFile.name;
-  const md5 = uploadFile.md5();
-  const saveAs = `${md5}_${name}`;
-  uploadFile.mv(`${__dirname}/public/files/${saveAs}`, function(err) {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    return res.status(200).json({ status: 'uploaded', name, saveAs });
-  });
-
   let postData = new Register(data);
   Register.findOne({
     email:data['email']
