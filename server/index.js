d4bbcce86f98789e64f78bfde522edd4252ce09f
AgusLasalvia <aguslblumenfeld@gmail.com>
2023-06-15 20:20:08 -0300

    .

--- server/index.js
+++ server/index.js
@@ -85,7 +85,7 @@ app.post("/get_personInfo", (req, res) => {
 });
 
 app.post("/registration", async (req, res) => {
-  const data = req.body;
+  const data = req.body.registration;
   const file = req.files;
   console.log(file)
   let postData = new Register(data);
