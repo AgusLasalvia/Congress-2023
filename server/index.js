e19f742a11a4bfc0e18c6f5bf96cd766ce0dcded
AgusLasalvia <aguslblumenfeld@gmail.com>
2023-06-18 21:44:33 -0300

    .

--- server/index.js
+++ server/index.js
@@ -144,7 +144,7 @@ app.post("/submit-abstract-data", (req, res) => {
 app.post("/submit-abstract-files",async (req, res) => {
   const files = req.files;
   await uploadFile(files.editableFormat)
-  await new uploadFile(fileUpload.pdfFormat)
+  await uploadFile(files.pdfFormat)
 
 
 
